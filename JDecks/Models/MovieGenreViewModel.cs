99925d305f8551ae9bf4a6bda6334569f05b486a
VB2090 <vicki_charlie@hotmail.com>
2017-11-11 11:35:52 +0000

    Updates for Search

---
+++ JDecks/Models/MovieGenreViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace JDecks.Models
+{
+    public class MovieGenreViewModel
+    {
+        public List<Decks> movies;
+        public SelectList genres;
+        public string movieGenre { get; set; }
+    }
+} 
